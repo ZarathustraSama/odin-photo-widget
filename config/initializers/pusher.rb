531a08348c863debb22cce9882c55050ed2addaa
ZarathustraSama <mr.gabriel@live.it>
2024-07-25 10:35:56 +0200

    Initial commit

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,5 @@
+# Configurated as per the gemfile's instructions here https://github.com/laserlemon/figaro
+
+Pusher.app_id = ENV["pusher_app_id"]
+Pusher.key = ENV["pusher_key"]
+Pusher.secret = ENV["pusher_secret"]
